2a150f3ea43e26ae881fd963cdd9ab2183da0ffc
Tugdual de Kerviler <dekervit@gmail.com>
2017-07-18 12:04:14 +0200

    Listen for follow command

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ const token = require( './secrets.json' ).BOT_TOKEN;
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot( token, { polling: true } );
 
+const chats = {};
+
+function hasJoinedChat( chatId ) {
+	return !! chats[ chatId ];
+}
+
+function joinChat( chat ) {
+	chats[ chat.id ] = chat;
+}
+
+function followBlog( url ) {
+	// TODO: get RSS content for this blog
+}
+
 // Matches "/echo [whatever]"
 bot.onText( /\/echo (.+)/, ( msg, match ) => {
 	// 'msg' is the received Message from Telegram
@@ -22,9 +36,25 @@ bot.onText( /\/echo (.+)/, ( msg, match ) => {
 // Listen for any kind of message. There are different kinds of
 // messages.
 bot.on( 'message', ( msg ) => {
-	const chatId = msg.chat.id;
+	if ( ! hasJoinedChat( msg.chat.id ) ) {
+		joinChat( msg.chat );
+	}
+
+	// ignore messages from groups
+	if ( msg.chat.type !== 'channel' ) {
+		return;
+	}
+
+	const reResult = /follow ((http|https):\/\/\S+)/gi.exec( 'follow https://stackoverflow.com/questions/4643' );
+
+	if ( reResult && reResult.length >= 2 ) {
+		const url = reResult[1];
+
+		// TODO: check that msg.from is admin on the channel
+		followBlog( url );
+	}
 
 	// send a message to the chat acknowledging receipt of their message
-	bot.sendMessage( chatId, 'Received your message' );
+	bot.sendMessage( msg.chat.id, 'Received your message' );
 } );
 
