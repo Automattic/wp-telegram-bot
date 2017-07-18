14f74c9f1e15e70b3218cacdf7838f4769b6dc7e
Yury Michurin <yury@automattic.com>
2017-07-18 23:22:18 +0300

    Make xmpp work

--- xmpp.js
+++ xmpp.js
@@ -1,3 +1,4 @@
+const debug = require( 'debug' )( 'wp-telegram-bot:xmpp' );
 const xmpp = require( 'xmpp.js' );
 const Client = xmpp.Client;
 const xml = xmpp.xml;
@@ -5,70 +6,66 @@ const xml = xmpp.xml;
 const secrets = require( './secrets.json' );
 const XMPP_USER = secrets.XMPP_USER;
 const XMPP_PASS = secrets.XMPP_PASS;
-const client = new Client();
-client.plugin( require('@xmpp/plugins/starttls') )
+const client = new Client( { jid: XMPP_USER, password: XMPP_PASS, preferred: 'PLAIN' } );
 
-client.start( { uri: 'xmpp://xmpp.wordpress.com', domain: 'im.wordpress.com' } )
+let newPostCallBack = null;
 
-client.on('error', err => {
-  console.error('❌', err.toString())
-})
+client.plugin( require('@xmpp/plugins/starttls') );
 
-client.on('status', (status, value) => {
-  console.log('🛈i status changed to', status, value ? value.toString() : '')
-})
+client.start( { uri: 'xmpp://xmpp.wordpress.com', domain: 'im.wordpress.com' } );
 
-// Useful for logging raw traffic
-// Emitted for every incoming fragment
-client.on('input', data => console.log('⮈', data))
-// Emitted for every outgoing fragment
-client.on('output', data => console.log('⮊', data))
-
+client.on('error', err => debug( err.toString() ) );
+client.on('status', ( status, value ) => debug( 'status changed to', status, value ? value.toString() : '') );
+client.on('close', debug( 'connection closed' ) );
 
 client.on('online', jid => {
-  console.log('🗸', 'online as', jid.toString())
-//	client.send(xml('presence'))
+	debug( 'online as ' + jid.toString() );
 
-setInterval( () => { client.send(xml('presence'))  }, 15 * 1000 );
-/*
-<iq from='juliet@example.com/balcony' type='set' id='roster_2'>
-  <query xmlns='jabber:iq:roster'>
-    <item jid='nurse@example.com'
-          name='Nurse'>
-      <group>Servants</group>
-    </item>
-  </query>
-</iq>
-
-
-	const message = xml( 'iq', {from: 'wpcomtelegrambot@im.wordpress.com', type: 'set', id: 'roster_1' },
-		xml( 'query', 'jabber:iq:roster',
-			xml( 'item', { jid: 'yurynix@im.wordpress.com', name: 'Yury' } )
+	// workaround for: https://github.com/xmppjs/xmpp.js/pull/369
+	// <iq type="set" id="5e5b6f40-5187-4373-9929-27c3149ca6ca-2"><session xmlns="urn:ietf:params:xml:ns:xmpp-session" /></iq>
+	client.sendReceive(
+		xml( 'iq', { type: 'set', id: 'session_set' },
+			xml( 'session', 'urn:ietf:params:xml:ns:xmpp-session' )
 		)
-	);
-
-    client.send( message ).then( d => console.log( 'd', d ), err => console.log( 'e', err ) );
-
-*/
-	const message2 = xml('message', {from: 'wpcomtelegrambot@im.wordpress.com', to: 'yurynix@im.wordpress.com', id: 'msg_1', type: 'chat' } , xml( 'body', null, 'howdy' ) );
-
-	client.send( message2 ).then( d => console.log( 'd', d ), err => console.log( 'e', err ) );
-
-	/*subscribe( 'scruffian.blog' ); */
-})
+	)
+	.then( data => client.send( xml( 'presence', { 'xml:lang': 'en' } ) ) );
+} );
 
 client.on('stanza', stanza => {
-  console.log('>> Stanza', stanza.toString())
-})
+	console.log('>> Stanza', stanza.toString())
+	if ( stanza.is( 'message' ) ) {
+		debug( 'got message ' + stanza.toString() );
+		const body = stanza.getChild( 'body' );
+
+		if ( body ) {
+			const messageText = body.getText();
+			const postUrlMatch = messageText.match(/(https?:\/\/.*$)/gi);
+			if ( postUrlMatch ) {
+				const postUrl = urlMatch[0];
+				const blogHost = stanza.attrs.from;
+
+				if ( typeof newPostCallBack === 'function' ) {
+					newPostCallBack( blogHost, postUrl );
+				}
+			}
+		}
+	}
+} );
 
 client.handle('authenticate', authenticate => authenticate( XMPP_USER, XMPP_PASS ) )
 client.handle('bind', bind => bind( 'bot' ) )
 
-function subscribe( blogHost ) {
-	const message = xml('message', {to: 'bot@im.wordpress.com '} , xml('body', 'sub ' + blogHost ) );
-	client.send( message ).then( d => console.log( 'd', d ), err => console.log( 'e', err ) ); 
+function sendMessage( to, text ) {
+	const message = xml('message', { to: to } , xml('body', null, text ) );
+	return client.send( message );
 }
 
+const subscribe = blogHost => sendMessage( 'bot@im.wordpress.com', 'sub ' + blogHost );
+const unsubscribe = blogHost => sendMessage( 'bot@im.wordpress.com', 'unsub ' + blogHost );
+const registerNewPostCallBack = callback => newPostCallBack = callback;
+
 module.exports = {
-	subscribe
+	subscribe,
+	unsubscribe,
+	registerNewPostCallBack
 };
