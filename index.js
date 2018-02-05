b7db355d6fd9996a578c1d5c4199e3c1a5065929
Yury Michurin <yury.michurin@gmail.com>
2018-02-05 09:49:03 +0200

    Revert identation of .then

--- index.js
+++ index.js
@@ -82,14 +82,14 @@ bot.on( 'message', msg => {
 	}
 
 	bot.getChatAdministrators( msg.chat.id )
-	.then( administrators => {
-		if ( administrators.filter( admin => admin.user.username === msg.from.username ).length === 0 ) {
-			return Promise.reject( new Error( 'You need to be an administrator of the channel to do that' ) );
-		}
-	} )
-	.then( () => followBlog( msg.chat.id, 'group', url ) )
-	.then( () => bot.sendMessage( msg.chat.id, 'Following!' ) )
-	.catch( error => handleError( error, msg.chat.id, url ) );
+		.then( administrators => {
+			if ( administrators.filter( admin => admin.user.username === msg.from.username ).length === 0 ) {
+				return Promise.reject( new Error( 'You need to be an administrator of the channel to do that' ) );
+			}
+		} )
+		.then( () => followBlog( msg.chat.id, 'group', url ) )
+		.then( () => bot.sendMessage( msg.chat.id, 'Following!' ) )
+		.catch( error => handleError( error, msg.chat.id, url ) );
 } );
 
 bot.on( 'channel_post', ( msg ) => {
